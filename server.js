df0cedd48287e4cca9cb25e4dede497db3b937a8
bkcpro <you@example.com>
2019-05-19 11:14:44 -0700

    package.json

--- server.js
+++ server.js
@@ -14,11 +14,15 @@ app.use((req, res, next) => {
   var now = new Date().toString();
   var log = `${now}: ${req.method} ${req.url}`;
 
-  console.log(log);
-  fs.appendFile('server.log', log + '\n');
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if(err){
+      console.log('Unable to append server.log');
+    }
+  });
   next();
 });
 
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
   res.render('portfolio.hbs', {
